a22e0fd5baec9b0fd0bfa98b3bf7b4fadc9552b6
Thomas Linkin <trlinkin@gmail.com>
2013-10-31 18:47:03 -0400

    Add ability to pass ssh_identity to default class

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,5 +1,6 @@
 class warm_standby (
   $remote_host,
+  $ssh_identity = undef,
   $databases = ['console', 'console_auth', 'pe-postgres', 'pe-puppetdb'],
   $replicate_folders = true,
   $folders = ['/etc/puppetlabs','/opt/puppet'],
@@ -11,14 +12,16 @@ class warm_standby (
   }
 
   warm_standby::replicate::database { $databases:
-    remote_host => $remote_host,
-    require     => Package['rsync'],
+    remote_host  => $remote_host,
+    ssh_identity => $ssh_identity,
+    require      => Package['rsync'],
   }
 
   if $replicate_folders {
     warm_standby::replicate::folder { $folders:
-      remote_host => $remote_host,
-      require     => Package['rsync'],
+      remote_host  => $remote_host,
+      ssh_identity => $ssh_identity,
+      require      => Package['rsync'],
     }
   }
 }
